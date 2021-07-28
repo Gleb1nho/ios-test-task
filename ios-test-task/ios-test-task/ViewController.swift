909bfaa09611e24663475927f05bc312afba66ca
pestretsov <pestretsov@skbkontur.com>
2021-07-28 14:48:43 +0500

    Project initialization

---
+++ ios-test-task/ios-test-task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-test-task
+//
+//  Created by Gleb Pestretsov on 28.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
