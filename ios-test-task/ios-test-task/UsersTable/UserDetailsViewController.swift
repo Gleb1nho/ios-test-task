701cab5792ea15ca5ea1fb413e8906bff39506bd
pestretsov <pestretsov@skbkontur.com>
2021-08-17 18:22:44 +0500

    Refactor, add bottomAnchor and scrollview

--- ios-test-task/ios-test-task/UsersTable/UserDetailsViewController.swift
+++ ios-test-task/ios-test-task/UsersTable/UserDetailsViewController.swift
@@ -1,14 +1,114 @@
 import UIKit
 
 class UserDetailsViewController: UIViewController, UITextViewDelegate {
-  private var userInfo: UserDBModel
-  private let scrollView: UIScrollView = {
+  private let userInfo: UserDBModel
+  
+  private let containerView: UIView = {
+    let view = UIView()
+    
+    view.translatesAutoresizingMaskIntoConstraints = false
+    view.backgroundColor = .white
+    
+    return view
+  }()
+  
+  private lazy var scrollView: UIScrollView = {
     let scrollView = UIScrollView()
     scrollView.translatesAutoresizingMaskIntoConstraints = false
     scrollView.backgroundColor = .white
+    scrollView.addSubview(containerView)
+    
     return scrollView
   }()
   
+  private lazy var nameLabel: UILabel = {
+    let nameLabel = UILabel()
+    
+    nameLabel.text = userInfo.name
+    nameLabel.numberOfLines = 0
+    nameLabel.textAlignment = .center
+    nameLabel.font = UIFont.systemFont(ofSize: 48)
+    nameLabel.sizeToFit()
+    nameLabel.adjustsFontSizeToFitWidth = true
+    nameLabel.translatesAutoresizingMaskIntoConstraints = false
+
+    return nameLabel
+  }()
+  
+  private lazy var dateOfEducationLabel: UILabel = {
+    let dateOfEducationLabel = UILabel()
+    
+    dateOfEducationLabel.text = "\(formateEducationDate(userInfo.educationPeriodStart))-\(formateEducationDate(userInfo.educationPeriodEnd))"
+    dateOfEducationLabel.textAlignment = .center
+    dateOfEducationLabel.font = UIFont.systemFont(ofSize: 24)
+    dateOfEducationLabel.numberOfLines = 0
+    dateOfEducationLabel.sizeToFit()
+    dateOfEducationLabel.textColor = .gray
+    dateOfEducationLabel.translatesAutoresizingMaskIntoConstraints = false
+
+    return dateOfEducationLabel
+  }()
+  
+  private lazy var temperLabel: UILabel = {
+    let temperLabel = UILabel()
+    
+    temperLabel.text = userInfo.temperament
+    temperLabel.textAlignment = .center
+    temperLabel.font = UIFont.systemFont(ofSize: 20)
+    temperLabel.numberOfLines = 0
+    temperLabel.sizeToFit()
+    temperLabel.textColor = .gray
+    temperLabel.translatesAutoresizingMaskIntoConstraints = false
+
+    return temperLabel
+  }()
+  
+  private lazy var phoneLabel: UITextView = {
+    let phoneLabel = UITextView()
+    
+    let attributedString = NSMutableAttributedString(string: "📞 \(userInfo.phone)")
+    attributedString.addAttribute(.link, value: "tel:+\(userInfo.formattedPhone)", range: NSRange(location: 0, length: 20))
+    
+    phoneLabel.attributedText = attributedString
+    phoneLabel.textAlignment = .center
+    phoneLabel.font = UIFont.systemFont(ofSize: 20)
+    phoneLabel.isScrollEnabled = false
+    phoneLabel.isEditable = false
+    phoneLabel.translatesAutoresizingMaskIntoConstraints = false
+
+    return phoneLabel
+  }()
+  
+  private lazy var biographyLabel: UILabel = {
+    let biographyLabel = UILabel()
+    
+    biographyLabel.text = "\(userInfo.biography)"
+    biographyLabel.numberOfLines = 0
+    biographyLabel.textAlignment = .center
+    biographyLabel.textColor = .gray
+    biographyLabel.sizeToFit()
+    biographyLabel.font = UIFont.systemFont(ofSize: 20)
+    biographyLabel.translatesAutoresizingMaskIntoConstraints = false
+
+    return biographyLabel
+  }()
+  
+  private lazy var topSeparator: UIView = {
+    let topSeparator = UIView()
+    topSeparator.backgroundColor = .gray
+    topSeparator.translatesAutoresizingMaskIntoConstraints = false
+
+    return topSeparator
+  }()
+  
+  private lazy var bottomSeparator: UIView = {
+    let botttomSeparator = UIView()
+    botttomSeparator.backgroundColor = .gray
+    botttomSeparator.translatesAutoresizingMaskIntoConstraints = false
+    
+    return botttomSeparator
+  }()
+
   init(userInfo: UserDBModel) {
     self.userInfo = userInfo
     super.init(nibName: nil, bundle: nil)
@@ -18,85 +118,35 @@ class UserDetailsViewController: UIViewController, UITextViewDelegate {
     fatalError("init(coder:) has not been implemented")
   }
   
-  
   override func viewDidLoad() {
-    setup()
     super.viewDidLoad()
+    setup()
   }
   
   private func formateEducationDate(_ educationDate: Date) -> String {
     let dateFormatterGet = DateFormatter()
     dateFormatterGet.dateFormat = "yyyy-MM-dd HH:mm:ss +HHmm"
-
+    
     let dateFormatterPrint = DateFormatter()
     dateFormatterPrint.dateFormat = "dd.MM.yyyy"
-
+    
     if let date = dateFormatterGet.date(from: "\(educationDate)") {
-        return dateFormatterPrint.string(from: date)
+      return dateFormatterPrint.string(from: date)
     } else {
-       return "\(educationDate)"
+      return "\(educationDate)"
     }
   }
   
   private func setup() {
-        
-    let nameLabel = UILabel()
-    let dateOfEducationLabel = UILabel()
-    let temperLabel = UILabel()
-    let phoneLabel = UITextView()
-    let biographyLabel = UILabel()
-    let topSeparator = UIView()
-    let botttomSeparator = UIView()
-    
-    topSeparator.backgroundColor = .gray
-    botttomSeparator.backgroundColor = .gray
-    
-    nameLabel.text = userInfo.name
-    nameLabel.numberOfLines = 0
-    nameLabel.textAlignment = .center
-    nameLabel.font = UIFont.systemFont(ofSize: 48)
-    nameLabel.adjustsFontSizeToFitWidth = true
-    
-    let attributedString = NSMutableAttributedString(string: "📞 \(userInfo.phone)")
-    attributedString.addAttribute(.link, value: "tel:+\(userInfo.formattedPhone)", range: NSRange(location: 0, length: 20))
-
-    dateOfEducationLabel.text = "\(formateEducationDate(userInfo.educationPeriodStart))-\(formateEducationDate(userInfo.educationPeriodEnd))"
-    dateOfEducationLabel.textAlignment = .center
-    dateOfEducationLabel.font = UIFont.systemFont(ofSize: 24)
-    dateOfEducationLabel.textColor = .gray
-    
-    temperLabel.text = userInfo.temperament
-    temperLabel.textAlignment = .center
-    temperLabel.font = UIFont.systemFont(ofSize: 20)
-    temperLabel.textColor = .gray
-    
-    phoneLabel.attributedText = attributedString
-    phoneLabel.textAlignment = .center
-    phoneLabel.font = UIFont.systemFont(ofSize: 20)
-    phoneLabel.isEditable = false
-    
-    biographyLabel.text = userInfo.biography
-    biographyLabel.numberOfLines = 0
-    biographyLabel.textAlignment = .center
-    biographyLabel.textColor = .gray
-    biographyLabel.font = UIFont.systemFont(ofSize: 20)
-    
-    nameLabel.translatesAutoresizingMaskIntoConstraints = false
-    dateOfEducationLabel.translatesAutoresizingMaskIntoConstraints = false
-    phoneLabel.translatesAutoresizingMaskIntoConstraints = false
-    biographyLabel.translatesAutoresizingMaskIntoConstraints = false
-    temperLabel.translatesAutoresizingMaskIntoConstraints = false
-    topSeparator.translatesAutoresizingMaskIntoConstraints = false
-    botttomSeparator.translatesAutoresizingMaskIntoConstraints = false
-    
-    scrollView.addSubview(nameLabel)
-    scrollView.addSubview(dateOfEducationLabel)
-    scrollView.addSubview(temperLabel)
-    scrollView.addSubview(topSeparator)
-    scrollView.addSubview(phoneLabel)
-    scrollView.addSubview(botttomSeparator)
-    scrollView.addSubview(biographyLabel)
     view.addSubview(scrollView)
+    
+    containerView.addSubview(nameLabel)
+    containerView.addSubview(dateOfEducationLabel)
+    containerView.addSubview(temperLabel)
+    containerView.addSubview(topSeparator)
+    containerView.addSubview(phoneLabel)
+    containerView.addSubview(bottomSeparator)
+    containerView.addSubview(biographyLabel)
     
     NSLayoutConstraint.activate([
       scrollView.topAnchor.constraint(equalTo: view.topAnchor),
@@ -104,36 +154,43 @@ class UserDetailsViewController: UIViewController, UITextViewDelegate {
       scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
       scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
       
-      nameLabel.topAnchor.constraint(equalToSystemSpacingBelow: scrollView.safeAreaLayoutGuide.topAnchor, multiplier: 3),
-      nameLabel.bottomAnchor.constraint(equalTo: dateOfEducationLabel.topAnchor, constant: -16),
-      nameLabel.centerXAnchor.constraint(equalTo: scrollView.readableContentGuide.centerXAnchor),
-      nameLabel.widthAnchor.constraint(equalTo: scrollView.readableContentGuide.widthAnchor),
+      scrollView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+      scrollView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
+      scrollView.topAnchor.constraint(equalTo: containerView.topAnchor),
+      scrollView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
+      
+      scrollView.widthAnchor.constraint(equalTo: containerView.widthAnchor),
+
+      nameLabel.topAnchor.constraint(equalToSystemSpacingBelow: containerView.safeAreaLayoutGuide.topAnchor, multiplier: 3),
+      nameLabel.bottomAnchor.constraint(equalTo: dateOfEducationLabel.topAnchor),
+      nameLabel.centerXAnchor.constraint(equalTo: containerView.readableContentGuide.centerXAnchor),
+      nameLabel.widthAnchor.constraint(equalTo: containerView.readableContentGuide.widthAnchor),
       
       dateOfEducationLabel.bottomAnchor.constraint(equalTo: temperLabel.topAnchor),
-      dateOfEducationLabel.centerXAnchor.constraint(equalTo: scrollView.readableContentGuide.centerXAnchor),
-      dateOfEducationLabel.widthAnchor.constraint(equalTo: scrollView.readableContentGuide.widthAnchor),
+      dateOfEducationLabel.centerXAnchor.constraint(equalTo: containerView.readableContentGuide.centerXAnchor),
+      dateOfEducationLabel.widthAnchor.constraint(equalTo: containerView.readableContentGuide.widthAnchor),
       
       temperLabel.bottomAnchor.constraint(equalTo: topSeparator.topAnchor, constant: -24),
-      temperLabel.centerXAnchor.constraint(equalTo: scrollView.readableContentGuide.centerXAnchor),
-      temperLabel.widthAnchor.constraint(equalTo: scrollView.readableContentGuide.widthAnchor),
+      temperLabel.centerXAnchor.constraint(equalTo: containerView.readableContentGuide.centerXAnchor),
+      temperLabel.widthAnchor.constraint(equalTo: containerView.readableContentGuide.widthAnchor),
       
       topSeparator.bottomAnchor.constraint(equalTo: phoneLabel.topAnchor, constant: -8),
-      topSeparator.centerXAnchor.constraint(equalTo: scrollView.readableContentGuide.centerXAnchor),
-      topSeparator.widthAnchor.constraint(equalTo: scrollView.readableContentGuide.widthAnchor),
+      topSeparator.centerXAnchor.constraint(equalTo: containerView.readableContentGuide.centerXAnchor),
+      topSeparator.widthAnchor.constraint(equalTo: containerView.readableContentGuide.widthAnchor),
       topSeparator.heightAnchor.constraint(equalToConstant: 1 / UIScreen.main.scale),
       
-      phoneLabel.bottomAnchor.constraint(equalTo: botttomSeparator.topAnchor, constant: -8),
-      phoneLabel.centerXAnchor.constraint(equalTo: scrollView.readableContentGuide.centerXAnchor),
-      phoneLabel.widthAnchor.constraint(equalTo: scrollView.readableContentGuide.widthAnchor),
-      phoneLabel.heightAnchor.constraint(equalTo: scrollView.readableContentGuide.heightAnchor, multiplier: 0.07),
+      phoneLabel.bottomAnchor.constraint(equalTo: bottomSeparator.topAnchor, constant: -8),
+      phoneLabel.centerXAnchor.constraint(equalTo: containerView.readableContentGuide.centerXAnchor),
+      phoneLabel.widthAnchor.constraint(equalTo: containerView.readableContentGuide.widthAnchor),
       
-      botttomSeparator.bottomAnchor.constraint(equalTo: biographyLabel.topAnchor, constant: -16),
-      botttomSeparator.centerXAnchor.constraint(equalTo: scrollView.readableContentGuide.centerXAnchor),
-      botttomSeparator.widthAnchor.constraint(equalTo: scrollView.readableContentGuide.widthAnchor),
-      botttomSeparator.heightAnchor.constraint(equalToConstant: 1 / UIScreen.main.scale),
+      bottomSeparator.bottomAnchor.constraint(equalTo: biographyLabel.topAnchor, constant: -24),
+      bottomSeparator.centerXAnchor.constraint(equalTo: containerView.readableContentGuide.centerXAnchor),
+      bottomSeparator.widthAnchor.constraint(equalTo: containerView.readableContentGuide.widthAnchor),
+      bottomSeparator.heightAnchor.constraint(equalToConstant: 1 / UIScreen.main.scale),
       
-      biographyLabel.centerXAnchor.constraint(equalTo: scrollView.readableContentGuide.centerXAnchor),
-      biographyLabel.widthAnchor.constraint(equalTo: scrollView.readableContentGuide.widthAnchor)
+      biographyLabel.centerXAnchor.constraint(equalTo: containerView.readableContentGuide.centerXAnchor),
+      biographyLabel.widthAnchor.constraint(equalTo: containerView.readableContentGuide.widthAnchor),
+      biographyLabel.bottomAnchor.constraint(equalTo: containerView.bottomAnchor)
     ])
   }
   
